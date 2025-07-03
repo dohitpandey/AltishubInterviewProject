e419d066610c269b44765fc36c32b33f95085eb4
Dohit <dohitp@gmail.com>
2025-07-04 01:57:57 +0530

    Added Pagination, Sorting and Filtering

--- Catalogue/Apartments/models.py
+++ Catalogue/Apartments/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 
 class ApartmentModel(models.Model):
-    apartment_name = models.CharField(max_length=50)
+    apartment_name = models.CharField(max_length=100, null=False, blank=False)
+    city = models.CharField(max_length=50, null=False, blank=False)
     rent = models.DecimalField(
         max_digits=5,
         decimal_places=0,
@@ -18,6 +19,12 @@ class ApartmentModel(models.Model):
             MinValueValidator(1),
             MaxValueValidator(3)]
     )
+    posted_on = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.city:
+            self.city = self.city.title()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.apartment_name
--- Catalogue/Apartments/views.py
+++ Catalogue/Apartments/views.py
@@ -7,3 +7,26 @@ from .models import ApartmentModel
 class ApartmentViewSet(viewsets.ModelViewSet):
     queryset = ApartmentModel.objects.all()
     serializer_class = ApartmentSerializer
+
+    def get_queryset(self):
+
+        queryset = ApartmentModel.objects.all()
+
+        # filtering
+        city = self.request.query_params.get('city')
+        if city:
+            queryset = queryset.filter(city__iexact=city)
+        min_rent = self.request.query_params.get('min_rent')
+        if min_rent:
+            queryset = queryset.filter(rent__gte=min_rent)
+        max_rent = self.request.query_params.get('max_rent')
+        if max_rent:
+            queryset = queryset.filter(rent__lte=max_rent)
+
+        # sorting
+        allowed_fields = ['apartment_name', 'city', 'rent', 'bedrooms', 'posted_on']
+        sort_by = self.request.query_params.get('sort_by', '-posted_on')
+        sort_field = sort_by.lstrip('-')
+        if sort_field not in allowed_fields:
+            sort_by = '-posted_on'
+        return queryset.order_by(sort_by, '-posted_on')
